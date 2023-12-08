45e977ee75d1c657d6d8cd697866e332a3d46724
RParker505 <rchiado75@gmail.com>
2023-12-08 06:50:35 -0700

    Change let to const for pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Height is in m
 
-let pokemonList = [
+const pokemonList = [
     {name: 'Venusaur', height: 2, types: ['grass', 'poison']},
     {name: 'Pidgey', height: 0.3, types: ['flying', 'normal']},
     {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
