fc54ed45c6298d207f939f39d121f3201c438144
RParker505 <rchiado75@gmail.com>
2023-12-10 13:49:05 -0700

    Draft loops for pokemonList array
    Write one loop to write name, height on DOM, another to highlight a large pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,16 @@ const pokemonList = [
     {name: 'Pidgey', height: 0.3, types: ['flying', 'normal']},
     {name: 'Jigglypuff', height: 0.5, types: ['fairy', 'normal']}
 ];
+
+// Loop iterates over each item in pokemonList array
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<p>pokemonList[i].name' + ' (height:' + 'pokemonList[i].height' + ')');
+}
+
+// Highlight a large pokemon
+for (let i = 0; i < pokemonList.length; i++){
+    if (pokemonList[i].height > 1){
+      console.log(pokemonList[i].name + " is a big Pokemon!");
+      document.write('<p>pokemonList[i].name' + ' is a big Pokemon');
+    }
+  }
