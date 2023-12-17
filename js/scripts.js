03a23576a67886479b0a7b4b353a7d462eab95c5
RParker505 <rchiado75@gmail.com>
2023-12-17 13:45:57 -0700

    Add class name to button element

--- js/scripts.js
+++ js/scripts.js
@@ -34,4 +34,5 @@ pokemonRepository.getAll().forEach(function(pokemon) {
   let listItem = document.createElement('li');
   let button = document.createElement('button');
   button.innerText = pokemon.name;
+  button.classList.add('pokemon-button');
 });
