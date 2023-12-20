a7ea814adc4e941e722e9bb472a93345de8c3aa3
RParker505 <rchiado75@gmail.com>
2023-12-20 06:30:12 -0700

    Create loadDetails function in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -58,10 +58,25 @@ let pokemonRepository = (function () {
     })
   }
 
+  function loadDetails(item) {
+    let url = item.details;
+    return fetch(url).then(function (response){
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
-    loadList: loadList
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 
 })();
