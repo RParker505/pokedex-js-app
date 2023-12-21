d834ed0fc3c638724155f9563f496550be970b23
RParker505 <rchiado75@gmail.com>
2023-12-21 06:56:27 -0700

    Adjust apiUrl variable to remove limit

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let pokemonRepository = (function () {
 
   let pokemonList = [];
 
-  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
   function getAll() {
     return pokemonList;
